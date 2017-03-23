7b43e3964f4cb72cccd94eb7c51c1fce31b20bcc
Nivelais Quentin <quentin@nivelais.com>
2017-03-23 18:37:48 +0100

    ajout de Gson

--- app/src/main/java/com/iutnantes/nivelais_rialet/projetandroid/RequetteManager.java
+++ app/src/main/java/com/iutnantes/nivelais_rialet/projetandroid/RequetteManager.java
@@ -9,4 +9,43 @@ package com.iutnantes.nivelais_rialet.projetandroid;
 
 public class RequetteManager {
 
+    private String title;
+    private int ageMax;
+    private int numberPerPage;
+    private double minPopularity;
+    private String language;
+
+    public RequetteManager(String title, int ageMax, int numberPerPage, double minPopularity, String language) {
+        this.title = title;
+        this.ageMax = ageMax;
+        this.numberPerPage = numberPerPage;
+        this.minPopularity = minPopularity;
+        this.language = language;
+    }
+
+    public RequetteManager() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getAgeMax() {
+        return ageMax;
+    }
+
+    public int getNumberPerPage() {
+        return numberPerPage;
+    }
+
+    public double getMinPopularity() {
+        return minPopularity;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+
 }
